5a683a6718ca7d89ccf6f4c5ede9b4ab8a0d9a65
Simon Braitsch <S.Braitsch@gmx.de>
2024-07-23 22:29:34 +0200

    added CLI arg for submitting, adjusted template to return tuple of solutions

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,9 +1,20 @@
 #![allow(unused)]
+
+use std::env;
+
 mod aoc_2021;
 mod aoc_2022;
 mod aoc_2023;
 mod utils;
 
 fn main() {
-    aoc_2021::day_03::solve();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 3 && (&args[1] == "-s" || &args[1] == "--submit") {
+        // reqwest to submit part arg[2] of currently used day
+    } else if args.len() == 1 {
+        aoc_2021::day_03::solve();
+    } else {
+        eprintln!("Usage: {} (<[-s | --submit]> <part>)", args[0]);
+    }
 }
