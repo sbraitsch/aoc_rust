94c3f0767eba9bc0afdbc6a05b1f64c045450379
s-braitsch <simon.braitsch@novatec-gbmh.de>
2024-07-23 17:33:17 +0200

    some solutions for 2021, more work for templater

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,8 +1,9 @@
+#![allow(unused)]
 mod aoc_2021;
 mod aoc_2022;
 mod aoc_2023;
 mod utils;
 
 fn main() {
-    aoc_2021::day_01::solve();
+    aoc_2021::day_03::solve();
 }
--- templar/src/main.rs
+++ templar/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, error::Error, fs, path::Path};
+use std::{env, error::Error, fs, io::ErrorKind, path::Path};
 
 use reqwest::header::{HeaderMap, HeaderValue, COOKIE};
 
@@ -13,53 +13,59 @@ fn main() {
     }
 
     let year = &args[1];
-    let filename = &args[2];
+    let day = &args[2];
     let session = &args[3];
 
-    if let Err(e) = write_template_to_file(year, filename, session) {
-        eprintln!("Error: {}", e);
+    let base_path = format!("aoc/src/aoc_{year}");
+
+    if let Err(e) = write_solution_template(&base_path, year, day) {
+        eprintln!("Error writing solution template: {}", e);
+        std::process::exit(1);
+    }
+    if let Err(e) = write_mod(&base_path, day) {
+        eprintln!("Error updating mod.rs: {}", e);
+        std::process::exit(1);
+    }
+    if let Err(e) = write_input(&base_path, year, day, session) {
+        eprintln!("Error writing input file: {}", e);
         std::process::exit(1);
     }
 }
 
-fn write_template_to_file(year: &str, day: &str, session: &str) -> Result<(), Box<dyn Error>> {
+fn write_solution_template(base_path: &str, year: &str, day: &str) -> Result<(), Box<dyn Error>> {
     let content = TEMPLATE.replace("{{year}}", year).replace("{{day}}", day);
-    let in_dir = format!("aoc/src/aoc_{year}/inputs");
-    let sol_dir = format!("aoc/src/aoc_{year}/solutions");
-    let dir_path = Path::new(&sol_dir);
     let filename = format!("day_{day}.rs");
-    let file_path = dir_path.join(&filename);
-    if !file_path.exists() {
-        fs::write(file_path, content)?;
-        update_mod(dir_path, day)?;
-        download_input(Path::new(&in_dir), year, day, session)?;
-        println!("Created new file {filename} for AoC {year}");
-    } else {
-        println!("File already exists.")
-    }
-
+    let file_path = Path::new(&base_path).join("solutions").join(&filename);
+    write_file(&file_path, &content)?;
+    println!("<Created new file {filename} for AoC {year}>");
     Ok(())
 }
 
-fn update_mod(dir: &Path, day: &str) -> Result<(), Box<dyn Error>> {
-    let mod_path = dir.join("mod.rs");
-
+fn write_mod(base_path: &str, day: &str) -> Result<(), Box<dyn Error>> {
+    let mod_path = Path::new(&base_path).join("solutions").join("mod.rs");
     let mut content = if mod_path.exists() {
         fs::read_to_string(&mod_path)?
     } else {
         String::new()
     };
-
-    let add_to_mod = format!("pub mod day_{};", day);
+    let add_to_mod = format!("pub mod day_{};\n", day);
     if !content.contains(&add_to_mod) {
         content.push_str(&add_to_mod);
         fs::write(mod_path, content)?;
+        println!("<Updated mod.rs>");
     }
 
     Ok(())
 }
 
-fn download_input(dir: &Path, year: &str, day: &str, session: &str) -> Result<(), Box<dyn Error>> {
+fn write_input(
+    base_path: &str,
+    year: &str,
+    day: &str,
+    session: &str,
+) -> Result<(), Box<dyn Error>> {
+    let filename = format!("input_{day}.txt");
+    let file_path = Path::new(&base_path).join("inputs").join(&filename);
     let d = day.parse::<i32>().unwrap();
     let url = format!("https://adventofcode.com/{year}/day/{d}/input");
     let client = reqwest::blocking::Client::builder()
@@ -68,13 +74,17 @@ fn download_input(dir: &Path, year: &str, day: &str, session: &str) -> Result<()
     let mut headers = HeaderMap::new();
     headers.insert(COOKIE, HeaderValue::from_str(session)?);
     let response = client.get(url).headers(headers).send()?;
-    let filename = format!("input_{day}.txt");
-    let file_path = dir.join(&filename);
-    if !file_path.exists() {
-        fs::write(file_path, response.text()?)?;
-        println!("Retrieved input file for AoC {year}, Day {day}.");
+    write_file(&file_path, &response.text()?)?;
+    println!("<Retrieved puzzle input>");
+    Ok(())
+}
+
+fn write_file(full_path: &Path, content: &str) -> Result<(), Box<dyn Error>> {
+    if !full_path.exists() {
+        fs::write(full_path, content)?;
     } else {
-        println!("File already exists.")
+        let err = std::io::Error::new(ErrorKind::AlreadyExists, "File already exists");
+        return Err(Box::new(err));
     }
     Ok(())
 }
